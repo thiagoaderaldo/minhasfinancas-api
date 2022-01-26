7736fa044ebb561aae7a1773e8b3399828af78cb
thiagoaderaldo <thiagoaderaldo@gmail.com>
2022-01-26 17:36:09 -0300

    Aula 19 - Mapeamento de entidades

---
+++ src/test/java/com/tal/minhasfinancas/MinhasfinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tal.minhasfinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasfinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
