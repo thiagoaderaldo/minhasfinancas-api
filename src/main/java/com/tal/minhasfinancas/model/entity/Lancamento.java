2852e88ed24cf2b92d2099b4fdb33dc58cbbdd29
thiagoaderaldo <thiagoaderaldo@gmail.com>
2022-01-27 17:02:40 -0300

    Aula 20 - Mapeando as Enumerações na entidade Lancamento

--- src/main/java/com/tal/minhasfinancas/model/entity/Lancamento.java
+++ src/main/java/com/tal/minhasfinancas/model/entity/Lancamento.java
@@ -4,13 +4,21 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import com.tal.minhasfinancas.model.enums.StatusLancamento;
+import com.tal.minhasfinancas.model.enums.TipoLancamento;
 
 @Entity
 @Table(name = "lancamento", schema = "financas")
@@ -20,6 +28,9 @@ public class Lancamento {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
+	
+	@Column(name = "descricao")
+	private String descricao;
 	
 	@Column(name = "mes")
 	private Integer mes;
@@ -35,5 +46,14 @@ public class Lancamento {
 	private BigDecimal valor;
 	
 	@Column(name = "data_cadastro")
+	@Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
 	private LocalDate dataCadastro;
+	
+	@Column(name = "tipo")
+	@Enumerated(value = EnumType.STRING)
+	private TipoLancamento tipo;
+	
+	@Column(name = "status")
+	@Enumerated(value = EnumType.STRING)
+	private StatusLancamento status;
 }
