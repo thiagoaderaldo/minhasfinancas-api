295b044b678865fdd3fd6dda27a79735850a2c04
thiagoaderaldo <thiagoaderaldo@gmail.com>
2022-01-28 16:34:38 -0300

    Aula 28 - Criacao do servico de Usuario

---
+++ src/main/java/com/tal/minhasfinancas/service/UsuarioService.java
@@ -0,0 +1,12 @@
+package com.tal.minhasfinancas.service;
+
+import com.tal.minhasfinancas.model.entity.Usuario;
+
+public interface UsuarioService {
+
+	Usuario autenticar(String email, String senha);
+	
+	Usuario salvarUsuario(Usuario usuario);
+	
+	void validarEmail(String email);
+}
---
+++ src/main/java/com/tal/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,26 @@
+package com.tal.minhasfinancas.service.impl;
+
+import com.tal.minhasfinancas.model.entity.Usuario;
+import com.tal.minhasfinancas.service.UsuarioService;
+
+public class UsuarioServiceImpl implements UsuarioService{
+
+	@Override
+	public Usuario autenticar(String email, String senha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario salvarUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void validarEmail(String email) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
