686f198122b8b0ebcf421b4be052371367da575b
Suporte <thiagoaderaldo@gmail.com>
2022-01-31 15:48:26 -0300

    Modificações no projeto para trabalhar com JUnit5

--- src/test/java/com/tal/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/tal/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -1,16 +1,16 @@
 package com.tal.minhasfinancas.model.repository;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.tal.minhasfinancas.model.entity.Usuario;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 public class UsuarioRepositoryTest {
 
 	@Autowired
