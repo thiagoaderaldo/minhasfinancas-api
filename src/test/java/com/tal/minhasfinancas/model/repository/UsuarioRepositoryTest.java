07bd66d4d0d4772a236a45de16c1506c50967b74
Suporte <thiagoaderaldo@gmail.com>
2022-02-01 13:28:55 -0300

    Aula 34 - Configurações para trabalhar com o H2DB

--- src/test/java/com/tal/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/tal/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.tal.minhasfinancas.model.entity.Usuario;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
 	@Autowired
