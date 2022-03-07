70371adf8c8f68e56a047df162ab1b4ae19716b0
Suporte <thiagoaderaldo@gmail.com>
2022-03-07 15:43:35 -0300

    Aula 55 - Implementação do método salvar do tipo POST em UsuarioResource

--- src/main/java/com/tal/minhasfinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/tal/minhasfinancas/api/resource/UsuarioResource.java
@@ -1,13 +1,41 @@
 package com.tal.minhasfinancas.api.resource;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tal.minhasfinancas.api.dto.UsuarioDTO;
+import com.tal.minhasfinancas.exception.RegraNegocioException;
+import com.tal.minhasfinancas.model.entity.Usuario;
+import com.tal.minhasfinancas.service.UsuarioService;
+
 @RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioResource {
 
-	@GetMapping("/")
-	public String helloWorld() {
-		return "Hello World!";
+	
+	private UsuarioService service;
+	
+	@PostMapping
+	public ResponseEntity salvar( @RequestBody UsuarioDTO dto) {
+		
+		Usuario usuario = Usuario.builder()
+				.email(dto.getEmail())
+				.nome(dto.getNome())
+				.senha(dto.getSenha())
+				.build();
+		
+		try {
+			
+			Usuario usuarioSalvo = service.salvarUsuario(usuario);
+			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+			
+		}catch (RegraNegocioException e) {
+			
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
 	}
 }
