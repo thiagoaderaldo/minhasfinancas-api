970937bd0f7dfd6a4ccc81b575bd2fd86816e76d
Suporte <thiagoaderaldo@gmail.com>
2022-03-10 16:48:21 -0300

    Aula 60 - Criação de serviço para Lançamentos

---
+++ src/main/java/com/tal/minhasfinancas/service/LancamentoService.java
@@ -0,0 +1,19 @@
+package com.tal.minhasfinancas.service;
+
+import java.util.List;
+
+import com.tal.minhasfinancas.model.entity.Lancamento;
+import com.tal.minhasfinancas.model.enums.StatusLancamento;
+
+public interface LancamentoService {
+
+	Lancamento salvar(Lancamento lancamento);
+	
+	Lancamento atualizar(Lancamento lancamento);
+	
+	void deletar(Lancamento lancamento);
+	
+	List<Lancamento> buscar(Lancamento lancamentoFiltro);
+	
+	void atualizarStatus(Lancamento lancamento, StatusLancamento status);
+}
---
+++ src/main/java/com/tal/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,53 @@
+package com.tal.minhasfinancas.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tal.minhasfinancas.model.entity.Lancamento;
+import com.tal.minhasfinancas.model.enums.StatusLancamento;
+import com.tal.minhasfinancas.model.repository.LancamentoRepository;
+import com.tal.minhasfinancas.service.LancamentoService;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+
+	private LancamentoRepository repository;
+	
+	public LancamentoServiceImpl(LancamentoRepository repository) {
+		
+		this.repository = repository;
+	}
+	
+	@Override
+	public Lancamento salvar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Lancamento atualizar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
